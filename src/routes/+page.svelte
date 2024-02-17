c508d53866884da542edab06168d4d8174deb362
zach-conrad <zconrad119@gmail.com>
2024-02-17 11:51:29 -0600

    inital

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    import logo from '../Assets/logo.png'
+</script>
+
+<nav class="bg-white flex items-center justify-between py-1 px-6 shadow-md">
+    <div class="flex-1 flex justify-start">
+        <img src={logo} alt="..." class="h-auto w-32 mb-4">
+    </div>
+    <ul class="flex-1 flex justify-center space-x-10 text-black text-lg font-body">
+        <li>
+            <a href="/" class="hover:text-blue-600 transition-colors duration-150">SERVICES</a>
+        </li>
+        <li>
+            <a href="/">REVIEWS</a>
+        </li>
+        <li>
+            <a href="/">ABOUT</a>
+        </li>
+        <li>
+            <a href="/">CONTACT</a>
+        </li>
+    </ul>
+    <div class="flex-1"></div>
+</nav>
